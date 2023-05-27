e4edea7bab332f1cc59415aa4edb290b1cd762a0
LandonBuell <42503615+landonbuell@users.noreply.github.com>
2023-05-27 15:23:22 -0400

    Added ImageProcessor Solution and internal projects

---
+++ ImageProcessor/Segmentation/__init__.py
@@ -0,0 +1,16 @@
+"""
+    Repository:     Buell-MSES-Project
+    Solution:       ImageProcessing
+    Project:        Segmentation
+    Namespace:      N/A
+    File:           __init__.py
+    Author:         Landon Buell
+    Date:           May 2023
+"""
+
+
+
+"""
+    Author:         Landon Buell
+    Date:           May 2023
+"""
---
+++ ImageProcessor/Source/__main__.py
@@ -0,0 +1,16 @@
+"""
+    Repository:     Buell-MSES-Project
+    Solution:       ImageProcessing
+    Project:        Source
+    Namespace:      N/A
+    File:           __main__.py
+    Author:         Landon Buell
+    Date:           May 2023
+"""
+
+
+
+"""
+    Author:         Landon Buell
+    Date:           May 2023
+"""
