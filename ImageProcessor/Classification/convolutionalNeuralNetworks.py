1a8d83b9e98988370f5657be09facfcebedd6e15
LandonBuell <42503615+landonbuell@users.noreply.github.com>
2023-06-07 00:01:09 -0400

    Added module for pytorch convolutional neural networks

---
+++ ImageProcessor/Classification/convolutionalNeuralNetworks.py
@@ -0,0 +1,45 @@
+"""
+    Repository:     Buell-MSES-Project
+    Solution:       ImageProcessing
+    Project:        Classification
+    Namespace:      N/A
+    File:           convolutionalNeuralNetworks.py
+    Author:         Landon Buell
+    Date:           June 2023
+"""
+
+        #### IMPORTS ####
+
+import torch
+from torch._C import Module
+
+
+        #### FUNCTION DEFINITIONS ####
+
+class Conv2dVGG16(torch.nn,Module):
+    """ 2D Convolutional Neural Network inspired from the VGG-16 architecture """
+
+    def __init__(self):
+        """ Constructor """
+        super().__init__()
+        self._conv1A  = torch.nn.Conv2d()
+
+
+    def __del__(self):
+        """ Destructor """
+        super().__del__()
+
+    # Public Interface
+
+    def forward(self,x):
+        """ Module's forward-pass mechanism """
+
+
+
+        return x
+
+
+"""
+    Author:         Landon Buell
+    Date:           June 2023
+"""
--- ImageProcessor/Framework/classificationManager.py
+++ ImageProcessor/Framework/classificationManager.py
@@ -31,10 +31,15 @@ class ClassificationManager(manager.Manager):
                  app):  # imageProcessingApp.ImageProcessingApp
         """ Constructor """
         super().__init__(app,ClassificationManager.__NAME)
+        self._callbackGetModel  = None      # Callback that returns a "new" Classification Conv Neural Net
+        self._model             = None  
 
     def __del__(self):
         """ Destructor """
         pass
+
+    # Accessors
+
 
     # Public Interface
 
@@ -63,7 +68,24 @@ class ClassificationManager(manager.Manager):
         self._setShutdownFinished(True)
         return self._status
 
+    def exportClassifierModel(self) -> None:
+        """ Export the current classifier Model to the outputs folder """
+
+        return None
+
     # Private Interface 
+
+    def __getNewClassifierModel(self):
+        """ Invoke the callback that returns a new classifier Model """
+        if (self._callbackGetModel is None):
+            msg = "No callback is defined to fetch a classifier neural network model"
+            self.logMessage(msg)
+            raise RuntimeError(msg)
+        model = self._callbackGetModel.__call__()
+        return model
+
+
+
 
 
 """
